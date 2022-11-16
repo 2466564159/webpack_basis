b63f62b808c5962208b6ecfa13de67cd2c4a336b
The proletarian warrior <2466564159@qq.com>
2022-11-16 12:46:53 +0800

    开发环境配置

---
+++ 08.开发环境配置/src/js/index.js
@@ -0,0 +1,9 @@
+import '../css/index.less'
+import '../css/iconfont.css'
+
+function add(x, y) {
+  return x + y
+}
+
+console.log(add(1, 2))
+console.log(add(2, 2))
---
+++ 08.开发环境配置/webpack.config.js
@@ -0,0 +1,85 @@
+/*
+  开发环境配置：能让代码运行
+    运行项目指令：
+      webpack 会将打包结果输出出去
+      npx webpack-dev-server 只会在内存中编译打包，没有输出
+*/
+
+const { resolve } = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: './src/js/index.js',
+  output: {
+    filename: 'js/built.js',
+    path: resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.less$/,
+        use: ['style-loader', 'css-loader', 'less-loader']
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(jpg|png|gif)$/,
+        loader: 'url-loader',
+        options: {
+          // 图片大小小于8kb，就会被base64处理
+          // 优点: 减少请求数量（减轻服务器压力）
+          // 缺点：图片体积会更大（文件请求速度更慢）
+          limit: 8 * 1024,
+          // 问题：因为url-loader默认使用es6模块化解析，而html-loader引入图片是commonjs
+          // 解析时会出问题：src=[object Module]
+          // 解决：关闭url-loader的es6模块化，使用commonjs解析
+          esModule: false,
+          // 给图片进行重命名
+          // [hash:10]取图片的hash的前10位
+          // [ext]取文件原来扩展名
+          name: '[hash:10].[ext]',
+          // 指定输出目录
+          outputPath: 'imgs',
+        }
+      },
+      // 处理html中img
+      {
+        test: /\.html$/,
+        // 处理html文件的img图片（负责引入img，从而能被url-loader进行处理）
+        loader: 'html-loader'
+      },
+      // 处理其他资源
+      {
+        exclude: /\.(html|js|css|less|jpg|png|gif)/,
+        loader: 'file-loader',
+        options: {
+          name: '[hash:10].[ext]',
+          // 指定输出目录
+          outputPath: 'media'
+        }
+      }
+    ]
+  },
+  plugins: [
+    // 指定使用的html模板
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })
+  ],
+  mode: 'development',
+  // 开发服务器 devServer：用来自动化（自动编译，自动打开浏览器，自动刷新浏览器~~）
+  // 特点：只会在内存中编译打包，不会有任何输出
+  // 启动devServer指令为：npx webpack-dev-server
+  devServer: {
+    // 项目构建后路径
+    contentBase: resolve(__dirname, 'dist'),
+    // 启动gzip压缩
+    compress: true,
+    // 端口号
+    port: 3000,
+    // 自动打开浏览器
+    open: true
+  }
+}
